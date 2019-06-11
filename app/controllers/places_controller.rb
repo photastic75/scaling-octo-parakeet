e085e65fa2beec439d9f47b45322e86c4dc7e501
Charlie Fu <charlie.zhe.fu@gmail.com>
2019-06-11 01:49:50 +0000

    nomster lesson 13 create place form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,13 @@ class PlacesController < ApplicationController
 
   def index
 
-    @places = Place.all
+    @places = Place.page(params[:page]).per(5)
+    
+  end
+
+  def new
+
+    @place = Place.new
     
   end
 
