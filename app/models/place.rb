23d2d2487a82a4084ec904f9edecc9dd71c1af91
Charlie Fu <charlie.zhe.fu@gmail.com>
2019-06-18 22:29:11 +0000

    Lesson 39 Step 7 CarrierWave to S3

--- app/models/place.rb
+++ app/models/place.rb
@@ -10,4 +10,6 @@ class Place < ApplicationRecord
   validates :address, presence: true
   validates :description, presence: true
 
+  mount_uploader :picture, PictureUploader
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_18_005118) do
+ActiveRecord::Schema.define(version: 2019_06_18_022915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_06_18_005118) do
     t.integer "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
     t.index ["place_id"], name: "index_photos_on_place_id"
     t.index ["user_id", "place_id"], name: "index_photos_on_user_id_and_place_id"
   end
@@ -45,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_06_18_005118) do
     t.integer "user_id"
     t.float "latitude"
     t.float "longitude"
+    t.string "picture"
     t.index ["user_id"], name: "index_places_on_user_id"
   end
 
